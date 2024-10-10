01954ed3ac844738cf51f9d1d6169f95694f1661
bourbonboi <artemmine9292@gmail.com>
2024-10-10 16:34:50 +0900

    lesson 9

---
+++ app/retrofitdataclass/src/main/java/com/example/retrofitdataclass/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.retrofitdataclass
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.WindowCompat
+import com.example.retrofitdataclass.databinding.ActivityMainBinding
+import com.example.retrofitdataclass.retrofit.MainApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.weatherapi.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        binding.request.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                val api = retrofit.create(MainApi::class.java)
+                val model = api.getWeatherData(
+                    "17b3af9b366f4eb3bcd51435242109",
+                    "London",
+                    "3",
+                    "no",
+                    "no"
+                )
+                binding.temp.text = model.current.temp_c.toString()
+                binding.date.text = model.location.name
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "RetrofitLesson"
 include(":app")
+include(":app:retrofitdataclass")
